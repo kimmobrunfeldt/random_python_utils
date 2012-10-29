97a100aac2cba3b1c35d765f2cb53790896e6e2f
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2012-10-29 17:55:46 +0200

    added max_diff parameter

--- fuzzyfind.py
+++ fuzzyfind.py
@@ -8,7 +8,7 @@ Example:
     all_words = ['apple1', 'apple', 'greip', 'greb', 'grape',
                  'grabe', 'painapple', 'pinaple', 'pineapple']
 
-    find_best_matches(real_words, all_words) should return:
+    find_similar_words(real_words, all_words) should return:
 
     {
      'apple': 'apple', 'apple1': 'apple',
@@ -23,11 +23,14 @@ Example:
 import difflib
 
 
-def find_best_match(item, seq, diff_func):
+def find_best_match(item, seq, diff_func, max_diff=None):
     """Finds best match for item 'item' from iterable 'seq'.
-    diff_func is the function that compares the difference between two items,
-    it must return int/float. The smaller the number,
-    the more similar items are.
+    diff_func: Function that compares the difference between two items,
+               it must return int/float. The smaller the number,
+               the more similar items are.
+    max_diff: Optional number value. If the best found match differs from the
+              actual word more than this value(diff_func(a, b) > max_diff),
+              None is returned. Meaning that none of the words matches.
 
     Returns the item from 'seq' which had highest match.
     """
@@ -41,25 +44,32 @@ def find_best_match(item, seq, diff_func):
     # Finds the real word which has the biggest similarity ratio
     best_match = min(similarity_ratios, key=lambda x: x[0])
 
+    if max_diff is not None and best_match[0] > max_diff:
+        return None
+
     return best_match[1]
 
 
-def find_similar_word(word, words):
-    """Finds best match for word from words."""
+def find_similar_word(word, words, max_diff=None):
+    """Finds best match for word from words.
+    max_diff: See find_best_match()."""
     # Use difflib's SequenceMatcher as the diff_func.
+    # "As a rule of thumb, a ratio() value over 0.6 means the sequences
+    # are close matches". Therefore max_diff=0.4 would find close matches.
     func = lambda x, y: 1.0 - difflib.SequenceMatcher(None, x, y).ratio()
-    return find_best_match(word, words, func)
+    return find_best_match(word, words, func, max_diff=max_diff)
 
 
-def find_similar_words(real_words, all_words):
+def find_similar_words(real_words, all_words, max_diff=None):
     """For all words in all_words, finds the best match from real_words.
+    max_diff: See find_best_match().
     Returns 'many-to-one' dictionary. It is guaranteed that returned dictionary
     contains all words that are in all_words. You can access the best found
     match with k[word_from_all_words].
     """
     k = {}
     for word in all_words:
-        best_match = find_similar_word(word, real_words)
+        best_match = find_similar_word(word, real_words, max_diff=max_diff)
         k[word] = best_match
 
     return k
@@ -89,6 +99,9 @@ def main():
 
     print('All similar words:')
     print(find_similar_words(real_words, all_words))
+
+    print('\nAll similar words with max_diff specified:')
+    print(find_similar_words(real_words, all_words, max_diff=0.3))
 
     numbers = [1, 3, 10, 40, 100]
     all_numbers = [1, 1.93, 2.1, 2, 4, 6, 50, 150, 2000]
